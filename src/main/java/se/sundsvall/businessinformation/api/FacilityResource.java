02458f0ad4942aa7a159bb41456883b0ff7238ae
lsjolinder <linus.sjolinder@b3.se>
2023-02-21 16:52:14 +0100

    OpenApi documentation

--- src/main/java/se/sundsvall/businessinformation/api/FacilityResource.java
+++ src/main/java/se/sundsvall/businessinformation/api/FacilityResource.java
@@ -12,12 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 import se.sundsvall.businessinformation.service.ecos.FacilityService;
 
 import generated.se.sundsvall.forratt.Anlaggningar;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @Validated
 @RequestMapping("/anlaggningar")
-@Tag(name = "Anlaggningar", description = "Hämta ut information om anläggningar")
+@Tag(name = "Anlaggningar", description = "Anläggningar som står som registrerade och som inte har ett upphört datum ska kunna klassas om.")
 public class FacilityResource {
     
     private final FacilityService service;
@@ -28,7 +30,11 @@ public class FacilityResource {
     
     
     @GetMapping("{organisationsnummer}")
-    public ResponseEntity<List<Anlaggningar>> getAnlaggningar(@PathVariable String organisationsnummer) {
+    @Operation(summary = "Hämta aktiva företag/anläggningar med angivet organisationsnummer",
+        description = "Returnerar aktiva anläggningar med angivet organisationsnummer")
+    public ResponseEntity<List<Anlaggningar>> getAnlaggningar(
+        @Schema(description = "organisationsnummer för den eller de anläggningar som ska hämtas")
+        @PathVariable String organisationsnummer) {
         return ResponseEntity.ok(service.getFacilities(organisationsnummer));
     }
 }
